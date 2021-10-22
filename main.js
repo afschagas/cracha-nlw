44a0bec5ae3a92f8ce6e9a0327281893957102c5
Alexandre Chagas <afschagas@gmail.com>
2021-10-21 21:26:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const linkSocialMedia = {
+  github: 'afschagas',
+  youtube: 'jakelinygracielly'
+}
+
+function changeSocialMediaLinks() {
+  //document.getElementById('userName').textContent = 'Bia'
+  //userName.textContent = 'Ale'
+  for (let li of socilLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linkSocialMedia[social]}`
+
+    // alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linkSocialMedia.github}`
+
+  // Pegando dados do JSON
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
